3e69a605c93c7bf9e11ad42e0ae03729998606ff
Steven1991 <nguenkochimi@gmail.com>
2022-03-21 04:40:23 +0100

    first commit

---
+++ src/test/java/com/project/mabarba/MabarbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mabarba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MabarbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
